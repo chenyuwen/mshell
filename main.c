9a70db31cacafd586b4ec8c256ac2ae3bc251b43
chenyuwen <chenyuwen@meizu.com>
2016-06-24 19:47:18 +0800

    [CODE] 增加一个空格，让显示更人性化。

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, unsigned char **argv, unsigned char **envp)
 //	}
 
 	while(mshell.main_loop) {
-		printf("%s:%s%s", mshell.user->pw_name, mshell.cur_dir, 
+		printf("%s:%s%s ", mshell.user->pw_name, mshell.cur_dir, 
 			((mshell.user->pw_uid)?"$":"#"));
 		fflush(stdout);
 

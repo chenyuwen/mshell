1f3ff22143a26e92c3302842d6e35d858d2b4324
chenyuwen <chenyuwen@meizu.com>
2016-06-24 13:39:07 +0800

    [CODE] 有限地支持cd命令。

--- cd_cmd.c
+++ cd_cmd.c
@@ -4,6 +4,21 @@
 
 int cd_command(struct mshell *mshell, struct cmd *cmd)
 {
-	return 0;
+	unsigned char *path;
+	int ret = 0;
+
+	if(cmd->cmd[1] == NULL) {
+		path = "~";
+	} else {
+		path = cmd->cmd[1];
+	}
+
+	/*TODO:把'~'替换为用户路径*/
+
+	ret = chdir(path);
+	if(ret < 0) {
+		perror("mshell");
+	}
+	return ret;
 }
 
